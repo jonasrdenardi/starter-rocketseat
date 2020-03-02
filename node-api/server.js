216bfd289b9aba543a29f171c5e5fa197f49dcfd
Jonas R. Denardi <36958021+jonasrdenardi@users.noreply.github.com>
2020-03-02 00:32:14 -0300

    Continuação

--- node-api/server.js
+++ node-api/server.js
@@ -6,10 +6,19 @@ const requireDir = require('require-dir');
 const app = express();
 
 // Iniciando o BD
-mongoose.connect('mongodb+srv://jonas:147258@cluster0-9q0pm.mongodb.net/nodeapi', {
+
+// Web Mongo Atlas
+// mongoose.connect('mongodb+srv://jonas:147258@cluster0-9q0pm.mongodb.net/nodeapi', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// Docker
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
 requireDir('./src/models');
 
 const Product = mongoose.model('Product');
