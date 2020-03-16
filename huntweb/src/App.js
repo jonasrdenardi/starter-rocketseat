170b5a95c7113a6bd1177814d3afc151cff5fc87
Jonas R. Denardi <36958021+jonasrdenardi@users.noreply.github.com>
2020-03-16 00:05:14 -0300

    Continuação

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './componentes/Header';
+import './styles.css';
+import Main from './pages/main';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Rockeatseat</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main/>
+  </div>
+);
 
 export default App;
